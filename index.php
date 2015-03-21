6f58d239e941acfbe3e45954025d72c9fc6231da
Yan Chen <chen.yan.cncy@gmail.com>
2015-03-21 15:54:26 -0400

    add main page

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE HTML>
+<!--
+	Highlights by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Highlights by HTML5 UP</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.scrollex.min.js"></script>
+		<script src="js/jquery.scrolly.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<section id="header">
+				<header class="major">
+					<h1>First Response</h1>
+					<p>Let's control the Parkinson together</p>
+				</header>
+				<div class="container">
+					<ul class="actions">
+						<li><a href="#one" class="button special scrolly">Start</a></li>
+					</ul>
+				</div>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="main special">
+				<div class="container">
+					<span class="image fit primary"><img src="images/pic01.jpg" alt="" /></span>
+					<div class="content">
+						<header class="major">
+							<h2>Parkinson and Us</h2>
+						</header>
+						<p>Aliquam ante ac id. Adipiscing interdum lorem praesent fusce pellentesque arcu feugiat. Consequat sed ultricies rutrum. Sed adipiscing eu amet interdum lorem blandit vis ac commodo aliquet integer vulputate phasellus lorem ipsum dolor lorem magna consequat sed etiam adipiscing interdum.</p>
+					</div>
+					<a href="#footer" class="goto-next scrolly">Next</a>
+				</div>
+			</section>
+
+		<!--&lt;!&ndash; Two &ndash;&gt;-->
+			<!--<section id="two" class="main special">-->
+				<!--<div class="container">-->
+					<!--<span class="image fit primary"><img src="images/pic02.jpg" alt="" /></span>-->
+					<!--<div class="content">-->
+						<!--<header class="major">-->
+							<!--<h2>Stuff I do</h2>-->
+						<!--</header>-->
+						<!--<p>Consequat sed ultricies rutrum. Sed adipiscing eu amet interdum lorem blandit vis ac commodo aliquet vulputate.</p>-->
+						<!--<ul class="icons-grid">-->
+							<!--<li>-->
+								<!--<span class="icon major fa-camera-retro"></span>-->
+								<!--<h3>Magna Etiam</h3>-->
+							<!--</li>-->
+							<!--<li>-->
+								<!--<span class="icon major fa-pencil"></span>-->
+								<!--<h3>Lorem Ipsum</h3>-->
+							<!--</li>-->
+							<!--<li>-->
+								<!--<span class="icon major fa-code"></span>-->
+								<!--<h3>Nulla Tempus</h3>-->
+							<!--</li>-->
+							<!--<li>-->
+								<!--<span class="icon major fa-coffee"></span>-->
+								<!--<h3>Sed Feugiat</h3>-->
+							<!--</li>-->
+						<!--</ul>-->
+					<!--</div>-->
+					<!--<a href="#three" class="goto-next scrolly">Next</a>-->
+				<!--</div>-->
+			<!--</section>-->
+
+		<!--&lt;!&ndash; Three &ndash;&gt;-->
+			<!--<section id="three" class="main special">-->
+				<!--<div class="container">-->
+					<!--<span class="image fit primary"><img src="images/pic03.jpg" alt="" /></span>-->
+					<!--<div class="content">-->
+						<!--<header class="major">-->
+							<!--<h2>One more thing</h2>-->
+						<!--</header>-->
+						<!--<p>Aliquam ante ac id. Adipiscing interdum lorem praesent fusce pellentesque arcu feugiat. Consequat sed ultricies rutrum. Sed adipiscing eu amet interdum lorem blandit vis ac commodo aliquet integer vulputate phasellus lorem ipsum dolor lorem magna consequat sed etiam adipiscing interdum.</p>-->
+					<!--</div>-->
+					<!--<a href="#footer" class="goto-next scrolly">Next</a>-->
+				<!--</div>-->
+			<!--</section>-->
+
+		
+		<!-- Footer -->
+			<section id="footer">
+				<div class="container">
+					<header class="major">
+						<h2>Fill out the information</h2>
+					</header>
+					<form method="post" action="#">
+						<div class="row uniform">
+							<div class="6u 12u$(xsmall)"><input type="text" name="firstname" id="fname" placeholder="First Name" /></div>
+							<div class="6u$ 12u$(xsmall)"><input type="text" name="lastname" id="lname" placeholder="Last Name" /></div>
+							<div class="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
+							<div class="6u 12u$(xsmall)"><input type="text" name="phonenum" id="cell" placeholder="Phone number" /></div>
+							<div class="6u 12u$(xsmall)"><input type="text" name="height" id="hei" placeholder="Height (cm)" /></div>
+							<div class="6u 12u$(xsmall)"><input type="text" name="weight" id="wei" placeholder="Weight (kg)" /></div>
+							<div class="12u$">
+								<ul class="actions">
+									<li><input type="submit" value="Start Test" class="special" /></li>
+								</ul>
+							</div>
+						</div>
+					</form>
+				</div>
+				<footer>
+					<ul class="icons">
+						<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon alt fa-dribbble"><span class="label">Dribbble</span></a></li>
+						<li><a href="#" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li><li>Demo Images: <a href="http://unsplash.com">Unsplash</a></li>
+					</ul>
+				</footer>
+			</section>
+
+	</body>
+</html>
